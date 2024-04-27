1f12bc9e421c32525ff97ec22267d7f0c6128afa
MichaelSchoenburg <michaelschoenburg+git@gmail.com>
2024-04-27 20:00:50 +0200

    tailored script to fit N-able Automation Manager

--- RiverbirdInstaller.ps1
+++ RiverbirdInstaller.ps1
@@ -3,13 +3,7 @@
     Riverbird Installer
 
 .DESCRIPTION
-    PowerShell script intended to be used with a different RMM in order to install the Riverbird agent.
-
-.INPUTS
-    No parameters. Variables are supposed to be set by the rmm solution this script is used in.
-
-.OUTPUTS
-    None
+    PowerShell script intended to be used with N-able RMM in order to install the Riverbird agent.
 
 .LINK
     GitHub: https://github.com/MichaelSchoenburg/RiverbirdInstaller
@@ -23,156 +17,169 @@
     Performance Considerations: https://docs.microsoft.com/en-us/powershell/scripting/dev-cross-plat/performance/script-authoring-considerations?view=powershell-7.1
 #>
 
-#region FUNCTIONS
-<# 
-    Declare Functions
-#>
-
-function Write-ConsoleLog {
-    <#
-    .SYNOPSIS
-    Logs an event to the console.
-    
-    .DESCRIPTION
-    Writes text to the console with the current date (US format) in front of it.
-    
-    .PARAMETER Text
-    Event/text to be outputted to the console.
-    
-    .EXAMPLE
-    Write-ConsoleLog -Text 'Subscript XYZ called.'
-    
-    Long form
-    .EXAMPLE
-    Log 'Subscript XYZ called.
-    
-    Short form
-    #>
-    [alias('Log')]
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true,
-        Position = 0)]
-        [string]
-        $Text
-    )
-
-    # Save current VerbosePreference
-    # $VerbosePreferenceBefore = $VerbosePreference
-
-    # Enable verbose output
-    # $VerbosePreference = 'Continue'
-
-    # Write verbose output
-    Write-Output "$( Get-Date -Format 'MM/dd/yyyy HH:mm:ss' ) - $( $Text )"
-
-    # Restore current VerbosePreference
-    # $VerbosePreference = $VerbosePreferenceBefore
-}
-
-function Import-ModuleForSure {
-    <#
-    .SYNOPSIS
-        Import a module
-
-    .DESCRIPTION
-        Imports and installs a module if it's not imported or installed already. Taking into account NuGet.
-
-    .NOTES
-        This function is pipeline ready.
-
-    .PARAMETER Name
-        Name of the PowerShell module
-
-    .EXAMPLE
-        Import-ModuleForSure -Name Posh-SSH
-        Imports Posh-SSH for sure.
-    #>
-    
-    param (
-        [Parameter(
-            Mandatory,
-            ValueFromPipeline
-        )]
-        [string]
-        $Name
-    )
-    
-    begin {
-        if (-not (Get-PackageProvider -Name NuGet) ) {
-            Log 'Installing NuGet...'
-            Install-PackageProvider -Name NuGet -Force
-        }
-    }
-    
-    process {
-        Log 'Loading module $Name...'
-        if (Get-Module -Name $Name) {
-            Log "Module '$( $Name )' has been imported already."
-        } elseif (Get-Module -Name $Name -ListAvailable) {
-            Log 'Module '$( $Name )' has been installed already, but is not imported. Importing Module...'
-            Import-Module -Name $Name
-        } else {
-            Log "Installing Module '$( $Name )'..."
-            Install-Module -Name $Name -Force
-
-            Log "Importing Module '$( $Name )..."
-            Import-Module -Name $Name
-        }
-    }
-    
-    end {
-        
-    }
-}
-
-function Set-TailingSlash {
-    [CmdletBinding()]
-    param (
-        # Name of the variable (as string)
-        [Parameter(
-            Mandatory,
-            ValueFromPipelineByPropertyName)]
-        [string]
-        $VariableName,
-
-        # Either '\' or '/' (as char)
-        [Parameter(
-            ValueFromPipelineByPropertyName
-        )]
-        [char]
-        [ValidatePattern('\/|\\')]
-        $Slash = '\'
-    )
-    
-    begin {
-        
-    }
-    
-    process {
-        $var = Get-Variable -Name $VariableName
-        if ( 
-            -not ( 
-                $var.Value.EndsWith( $Slash )
-            )
-        ){ 
-            $Value = $var.Value + $Slash
-            Set-Variable -Name $VariableName -Value $Value -Scope Script
-        }
-    }
-    
-    end {
-        
-    }
-}
-
-#endregion FUNCTIONS
-#region INITIALIZATION
-<# 
-    Libraries, Modules, ...
+# Example arguments
+<#
+    -monitoringVersion '11.0.2401'
+    -webServiceUrl = 'https://portal.MyRiverbirdServer.com/rmm' 
+    -installationToken 'Grf0l9BrCcGbPkcqxeJtgQ9FqU9PpFDBy71cHZejk0kUjhtjjCGjyVlgCdINZp6L'
+    -FtpServerFqdn = 'MyServer.com'
+    -FtpUsername = 'MyUser'
+    -FtpPassword = 'MyP4$$vv0rd'
+    -NameInstallFile = 'Riverbird RMM Installer.exe'
+    -DirSrc = '/home/CenterMANAGEMENT'
+    -DirDest = 'C:\TSD.CenterVision\Software\Riverbird'
 #>
 
 try {
+    #region FUNCTIONS
+    <# 
+        Declare Functions
+    #>
+
+    function Write-ConsoleLog {
+        <#
+        .SYNOPSIS
+        Logs an event to the console.
+        
+        .DESCRIPTION
+        Writes text to the console with the current date (US format) in front of it.
+        
+        .PARAMETER Text
+        Event/text to be outputted to the console.
+        
+        .EXAMPLE
+        Write-ConsoleLog -Text 'Subscript XYZ called.'
+        
+        Long form
+        .EXAMPLE
+        Log 'Subscript XYZ called.
+        
+        Short form
+        #>
+        [alias('Log')]
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory = $true,
+            Position = 0)]
+            [string]
+            $Text
+        )
+
+        # Save current VerbosePreference
+        # $VerbosePreferenceBefore = $VerbosePreference
+
+        # Enable verbose output
+        # $VerbosePreference = 'Continue'
+
+        # Write verbose output
+        Write-Output "$( Get-Date -Format 'MM/dd/yyyy HH:mm:ss' ) - $( $Text )"
+
+        # Restore current VerbosePreference
+        # $VerbosePreference = $VerbosePreferenceBefore
+    }
+
+    function Import-ModuleForSure {
+        <#
+        .SYNOPSIS
+            Import a module
+
+        .DESCRIPTION
+            Imports and installs a module if it's not imported or installed already. Taking into account NuGet.
+
+        .NOTES
+            This function is pipeline ready.
+
+        .PARAMETER Name
+            Name of the PowerShell module
+
+        .EXAMPLE
+            Import-ModuleForSure -Name Posh-SSH
+            Imports Posh-SSH for sure.
+        #>
+        
+        param (
+            [Parameter(
+                Mandatory,
+                ValueFromPipeline
+            )]
+            [string]
+            $Name
+        )
+        
+        begin {
+            if (-not (Get-PackageProvider -Name NuGet) ) {
+                Log 'Installing NuGet...'
+                Install-PackageProvider -Name NuGet -Force
+            }
+        }
+        
+        process {
+            Log 'Loading module $Name...'
+            if (Get-Module -Name $Name) {
+                Log "Module '$( $Name )' has been imported already."
+            } elseif (Get-Module -Name $Name -ListAvailable) {
+                Log 'Module '$( $Name )' has been installed already, but is not imported. Importing Module...'
+                Import-Module -Name $Name
+            } else {
+                Log "Installing Module '$( $Name )'..."
+                Install-Module -Name $Name -Force
+
+                Log "Importing Module '$( $Name )..."
+                Import-Module -Name $Name
+            }
+        }
+        
+        end {
+            
+        }
+    }
+
+    function Set-TailingSlash {
+        [CmdletBinding()]
+        param (
+            # Name of the variable (as string)
+            [Parameter(
+                Mandatory,
+                ValueFromPipelineByPropertyName)]
+            [string]
+            $VariableName,
+
+            # Either '\' or '/' (as char)
+            [Parameter(
+                ValueFromPipelineByPropertyName
+            )]
+            [char]
+            [ValidatePattern('\/|\\')]
+            $Slash = '\'
+        )
+        
+        begin {
+            
+        }
+        
+        process {
+            $var = Get-Variable -Name $VariableName
+            if ( 
+                -not ( 
+                    $var.Value.EndsWith( $Slash )
+                )
+            ){ 
+                $value = $var.Value + $Slash
+                Set-Variable -Name $VariableName -Value $value -Scope Script
+            }
+        }
+        
+        end {
+            
+        }
+    }
+
+    #endregion FUNCTIONS
+    #region INITIALIZATION
+    <# 
+        Libraries, Modules, ...
+    #>
+
     # Check if PowerShell module for SFTP is installed already
     Import-ModuleForSure -Name 'Posh-SSH'
 
@@ -182,32 +189,9 @@ try {
         Declare local variables and global variables
     #>
 
-    # The following variables should be set through your rmm solution. 
-    # Here some examples of possible declarations with explanations for each variable.
-    # Tip: PowerShell variables are not case sensitive.
-
-    <#
-        # Example arguments
-        $ExitCodeSuccess = 0
-        $ExitCodeFail = 1001
-        $monitoringVersion = '11.0.2401'
-        $webServiceUrl1 = 'https://portal.MyRiverbirdServer.com/rmm'
-        $webServiceUrl2 = ''
-        $installationToken = 'Grf0l9BrCcGbPkcqxeJtgQ9FqU9PpFDBy71cHZejk0kUjhtjjCGjyVlgCdINZp6L'
-        $proxyUrl = ''
-        $proxyPort = ''
-        $proxyBypassOnLocal = ''
-        $proxyUseDefaultCredentials = ''
-        $proxyDomain = ''
-        $proxyUsername = ''
-        $proxyPassword = ''
-        $FtpServerFqdn = 'MyServer.com'
-        $FtpUsername = 'MyUser'
-        $FtpPassword = 'MyP4$$vv0rd'
-        $NameInstallFile = 'Riverbird RMM Installer.exe'
-        $DirSrc = '/home/CenterMANAGEMENT'
-        $DirDest = 'C:\TSD.CenterVision\Software\Riverbird'
-    #>
+    # Set exit codes
+    $ExitCodeSuccess = 0
+    $ExitCodeFail = 1001
 
     # Make sure paths contain a tailing slash
     Set-TailingSlash -VariableName 'DirSrc' -Slash '/'
@@ -221,7 +205,7 @@ try {
     $Arguments = @(
         'install',
         "-token $( $installationToken )",
-        "-url $( $webServiceUrl1 )",
+        "-url $( $WebServiceUrl )",
         "-version $( $monitoringVersion )"
     )
 
